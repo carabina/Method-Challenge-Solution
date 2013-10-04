2809aecc7f81e7614c6c94b2566aba7017989030
Eliot Arntz <arntzel@gmail.com>
2013-10-04 23:07:41 +0200

    Initial Commit

---
+++ Method Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Method Challenge Solution
+//
+//  Created by Eliot Arntz on 10/4/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
